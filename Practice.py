b6cd114019ca217b0764d0c1a16a9d8693c242c8
error500-error404 <the_adsl@hotmail.com>
2020-11-12 17:54:39 +0800

    Test Push

---
+++ Practice.py
@@ -0,0 +1,26 @@
+x = 5 
+print(x)
+
+# declare
+# 基本上都是宣告用，沒打也會預設先開好
+# variable 變數
+# assignment 設值指令: 遇到等號就是設值指令，把右邊存到左邊的意思
+
+# 資料型別
+# data types
+# 1.整數 integer int (1,2,3....)
+# 2.浮點數(小數) float (1.1,1.22,1.333...)
+# 3.布林值 boolean bool (true,false)
+# 4.字串 string str (文字)
+
+# price = 10
+# pen = 0.38
+# name = 'Allen'
+# rain = True
+# print(price,pen,name,rain)
+
+
+name = input('請輸入名字: ')
+
+print(name)
+print('嗨',name)
